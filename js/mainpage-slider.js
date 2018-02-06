308d15270f50a83ac4545251f4cd0f787d500497
Roman Horobets <roman.horobets@rolique.io>
2018-02-06 18:05:19 +0200

    mainpage slider content effect

--- js/mainpage-slider.js
+++ js/mainpage-slider.js
@@ -2,8 +2,13 @@
 
 var mainpageSlider = $('.mainpage-slider-block');
 
+mainpageSlider.on('init', function( event, slick ){
+  var content = slick.$slides[0];
+  content.querySelector('.mainpage-slider-block__content').classList.add('active');
+});
+
 mainpageSlider.slick({
-  // autoplay: true,
+  autoplay: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   infinite: true,
@@ -12,4 +17,14 @@ mainpageSlider.slick({
   dots: true,
   appendDots: '.mainpage-slider-block__dots-wrapper',
   cssEase: 'cubic-bezier(0.47, 0, 0.745, 0.715)'
+});
+
+mainpageSlider.on('beforeChange', function( event, slick, currentSlide ){
+  var content = slick.$slides[currentSlide];
+  content.querySelector('.mainpage-slider-block__content').classList.remove('active');
+});
+
+mainpageSlider.on('afterChange', function( event, slick, currentSlide ){
+  var content = slick.$slides[currentSlide];
+  content.querySelector('.mainpage-slider-block__content').classList.add('active');
 });
