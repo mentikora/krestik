d3862f8e02c3bc603d62f01487775aa1419cf653
Roman Horobets <roman.horobets@rolique.io>
2018-02-12 15:53:34 +0200

    mainpage slider responsive

--- js/product-preview-slider.js
+++ js/product-preview-slider.js
@@ -4,11 +4,30 @@ var productSlider = $('.product-slider');
 
 productSlider.slick({
   slidesToShow: 3,
-  slidesToScroll: 1,
+  slidesToScroll: 3,
   autoplay: false,
   infinite: false,
   arrows: true,
   speed: 600,
   dots: false,
-  cssEase: 'cubic-bezier(0.47, 0, 0.745, 0.715)'
+  cssEase: 'cubic-bezier(0.47, 0, 0.745, 0.715)',
+  prevArrow: '<button type="button" class="slick-prev"><i class="flaticon-arrows-1"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="flaticon-arrows"></i></button>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 569,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+  ]
 });
